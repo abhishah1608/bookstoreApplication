e64695c89d8e954d82de6c20ec9c5445aaa91e4f
abhishah1608 <abhishah1608@gmail.com>
2024-12-30 20:11:00 -0500

    c changes

--- src/components/PublicRoute.jsx
+++
@@ -1,5 +0,0 @@
-const PublicRoute = ({ children }) => {
-  return 1 === 1 ? children : <Navigate to="/" replace />;
-};
-
-export default PublicRoute;
--- src/components/RouteConfig.jsx
+++ src/components/RouteConfig.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 
 // import dependencies from the project.
 import PrivateRoute from "./PrivateRoute";
-import PublicRoute from "./PublicRoute";
 import CartHistory from "./CartHistory";
 import Cart from "./Cart";
 import LoginForm from "./LoginForm";
@@ -15,65 +14,53 @@ import PaymentStatus from "./PaymentStatus";
 
 function RouteConfig() {
   return (
-    <Routes>
-      <Route path="/" element={<LoginForm />} />
-      <Route
-        path="/paymentstatus/:sessionId"
-        element={
-          <PublicRoute>
-            <PaymentStatus />
-          </PublicRoute>
-        }
-      />
-      <Route
-        path="/register"
-        element={
-          <PublicRoute>
-            <Register />
-          </PublicRoute>
-        }
-      />
-      <Route
-        path="/booklist"
-        element={
-          <PrivateRoute>
-            <BookList />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/cart"
-        element={
-          <PrivateRoute>
-            <Cart />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/history"
-        element={
-          <PrivateRoute>
-            <CartHistory />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/logout"
-        element={
-          <PrivateRoute>
-            <Logout />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/paymentform"
-        element={
-          <PrivateRoute>
-            <PaymentForm />
-          </PrivateRoute>
-        }
-      />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/paymentstatus/:sessionId" element={<PaymentStatus />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/booklist"
+          element={
+            <PrivateRoute>
+              <BookList />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <PrivateRoute>
+              <Cart />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/history"
+          element={
+            <PrivateRoute>
+              <CartHistory />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/logout"
+          element={
+            <PrivateRoute>
+              <Logout />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/paymentform"
+          element={
+            <PrivateRoute>
+              <PaymentForm />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
